4b9027596b7a8c3aa06ae433dee3395c14b3d28a
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-07-14 19:14:06 +0300

    cut-unnesessory-thingth

--- src/index.js
+++ src/index.js
@@ -22,31 +22,7 @@ const refs = {
 
 refs.select.addEventListener('change', onSelect)
 
-// const BASE_URL = 'https://api.thecatapi.com/v1/'
-// const ENDPOINT = 'breeds'
-// const API_KEY = 'live_hj3bZ5YYrlS7jC7F7EuKKOPYrFuUayTJnyrIAMNiPu2sdFPugKifPlNkZu0uskLu'
-function catInfoDell() {
-     refs.catInfo.setAttribute('hidden', true);
-}
-function catInfoAdd() {
-    refs.catInfo.removeAttribute('hidden')
-}
-
-
-loaderDell()
-function loaderDell() {
-    refs.loader.setAttribute('hidden', true);
-    }
-function loaderAdd() {
-    refs.loader.removeAttribute('hidden')
-}
-
-function breedSelectDell() {
-     refs.select.setAttribute('hidden', true);
-}
-function breedSelectAdd() {
-    refs.select.removeAttribute('hidden')
-}
+addHiddenAtribute(refs.loader)
 
  fetchBreeds().then((data) => {
            
@@ -54,23 +30,21 @@ function breedSelectAdd() {
                 .map(({ id, name }) => `<option value='' disabled hidden selected>Select cat</option>
                 <option value='${id}'>${name}</option>`)
                  .join('');             
-     refs.select.insertAdjacentHTML('beforeend', catInfo);     
-     
-        }
+     refs.select.insertAdjacentHTML('beforeend', catInfo);   
+     }
         )
      .catch(err => {
-         
-         breedSelectDell()
-
-          Notiflix.Notify.failure( refs.error.removeAttribute('hidden'))
+         addHiddenAtribute(refs.select)     
+        Notiflix.Notify.failure(removeHiddenAtribute(refs.error))
         console.log(err);
         })
 
 
 function onSelect(event) {
-loaderAdd()
-             breedSelectDell()
-             catInfoDell()
+            removeHiddenAtribute(refs.loader)   
+            addHiddenAtribute( refs.select) 
+            addHiddenAtribute(refs.catInfo)
+            
     fetchCatByBreed(event.target.value).then((data) => {
             
         let { name, temperament, description } = data[0].breeds[0];
@@ -86,88 +60,22 @@ loaderAdd()
        </div>
         `
     }).catch(err => {
-        breedSelectDell()
-      Notiflix.Notify.failure( refs.error.removeAttribute('hidden'))
+addHiddenAtribute( refs.select)         
+        Notiflix.Notify.failure(removeHiddenAtribute(refs.error))            
         console.log(err);
     }).finally(() => {
-            loaderDell()
-            breedSelectAdd()
-        catInfoAdd()
+        addHiddenAtribute(refs.loader)        
+        removeHiddenAtribute(refs.select)        
+        removeHiddenAtribute(refs.catInfo)
         })
 }
 
 
+function removeHiddenAtribute(el) {
+    el.removeAttribute('hidden')
+}
 
-
-
-
-
-
-
-
-
-
-
-// const refs = {
-//     select: document.querySelector('.breed-select')
-// };
-
-// const BASE_URL = 'https://api.thecatapi.com/v1/'
-// const ENDPOINT = 'breeds'
-// const API_KEY = 'live_hj3bZ5YYrlS7jC7F7EuKKOPYrFuUayTJnyrIAMNiPu2sdFPugKifPlNkZu0uskLu'
-// let storedBreeds = []
-// function fetchBreeds() {
-//     return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`).then(resp => {
-//         if (!resp.ok) {
-//             throw new Error(resp.statusText)
-//         };
-//         return resp.json('');
-//     })
-//         .then((data) => {
-   
-//             //filter to only include those with an `image` object
-//             data = data.filter(img => img.image?.url != null)
-   
-//             storedBreeds = data;
-   
-//             for (let i = 0; i < storedBreeds.length; i++) {
-//                 const breed = storedBreeds[i];
-//                 let option = document.createElement('option');
-     
-//                 //skip any breeds that don't have an image
-//                 if (!breed.image) continue
-     
-//                 //use the current array index
-//                 option.value = i;
-//                 option.innerHTML = `${breed.name}`;
-//                 refs.select.appendChild(option);
-//             }
-//             //show the first breed by default
-  
-//         }).catch(err => console.log(err))
-// };
-
-
- 
-//  function fetchBreeds() {
-//      fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(response.statusText)
-//             }
-//         return response.json();
-//         })
-//          .then((data) => {
-            
-//             const catInfo = data
-//                 .map(({ id, name }) => `<option value='${id}'>${name}</option>`)
-//                 .join('');
-//             console.log(catInfo)
-//             refs.selectEl.insertAdjacentHTML('beforeend', catInfo);
-//         }
-//         )
-//         .catch(err => {
-//         console.log(err);
-//     })
-// }
+function addHiddenAtribute(el) {
+    el.setAttribute('hidden', true);
+}
 
